0a9f8a7f7f91ecb03e9a58655780269d0747de12
coulsonzero <coulson2021@163.com>
2026-07-01 10:45:23 +0800

    [commit]: update

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'http://localhost:5000/api/v1',
   timeout: 5000,
   withCredentials: true,
 })
