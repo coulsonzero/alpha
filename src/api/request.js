4d3154a7880f7b31eac87e95d6b8a2c46607aab7
coulsonzero <coulson2021@163.com>
2026-07-01 10:48:46 +0800

    [commit]: update

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const request = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
   timeout: 5000,
   withCredentials: true,
 })
